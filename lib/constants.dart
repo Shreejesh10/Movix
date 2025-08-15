b0fe454930afb928c1371bb39bfbd5aff9058f5d
Shreejesh Pathak <shreejeshpathak@gmail.com>
2025-08-15 11:33:17 +0545

    Changed content pref

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,6 @@ const String API_URL = "http://10.0.2.2:8000";
 const List<String> GENRES = [
   'Action', 'Adventure', 'Animation', 'Comedy',
   'Crime', 'Documentary', 'Drama', 'Family',
-  'Fantasy', 'History', 'Horror', 'Music', 'Mystery',
-  'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western'
+  'Fantasy', 'War','History', 'Horror', 'Music', 'Mystery',
+  'Romance', 'Science Fiction', 'Thriller','TV Movie', 'Western'
 ];
